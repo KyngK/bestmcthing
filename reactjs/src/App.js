c9bcd43f7a4d4ae1f392a52bc2473ad07eb71e4a
Ardun <ardunaline@gmail.com>
2023-01-29 23:38:39 +0300

    fix phone version

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -72,24 +72,23 @@ function App() {
       <div class={(light)?(""):("overlay")}>
       <div class="container-fluid">
           <div class="row">
-          <div class="col-md-2">
-            <input class="toggletheme" onClick={onCheck} type="image" src={(light)?("https://i.imgur.com/rvXYKZ9.png"):("https://i.imgur.com/OWXmoUk.png")} alt="toggle theme"></input>
+            <div class="center adj">
+              <input class="toggletheme" onClick={onCheck} type="image" src={(light)?("https://i.imgur.com/rvXYKZ9.png"):("https://i.imgur.com/OWXmoUk.png")} alt="toggle theme"></input>
             </div>
-		      <div class="center col-md-8">
-            <img class="title" alt="THE BEST THING" src="https://i.imgur.com/lHRDVRv.png"/>
-			      <div class="page-header">
-				      <h3 class={(light)?("txt-light"):("txt-dark")}>
-					      Which is better?
-				      </h3>
-  			    </div>
-	  	    </div>
-          <div class="col-md-2">
-            </div>
-	      </div>
+		        <div class="center adj">
+              <img class="title" alt="THE BEST THING" src="https://i.imgur.com/lHRDVRv.png"/>
+  			      <div class="page-header">
+	  			      <h3 class={(light)?("txt-light"):("txt-dark")}>
+		  			      Which is better?
+			  	      </h3>
+  			      </div>
+	  	      </div>
+	        </div>
+          
 	      <div class="row">
           <div class="center col-md-5">
             <div class="img">
-            <a target="_blank" rel="noopener noreferrer" href={data[0].url}><img alt={data[0].title} src={(data[0].image == null)? "https://i.imgur.com/OzRR1xv.jpg" : data[0].image.slice(0, -35)} class="thing" /></a>
+            <a target="_blank" rel="noopener noreferrer" href={data[0].url}><img alt={data[0].title} src={(data[0].image == null)? "https://i.imgur.com/OzRR1xv.jpg" : data[0].image.slice(0, -34)} class="thing" /></a>
             </div>
             <button type="button" disabled={disabled} onClick={() => vote(data[1].id, data[0].id, false)}>
               {data[0].title}
@@ -99,10 +98,10 @@ function App() {
             </h3>
 		      </div>
           <div class="vcenter center col-md-2">
-            <img alt="Versus" src="https://upload.wikimedia.org/wikipedia/commons/7/70/Street_Fighter_VS_logo.png" />
+            <img class="vs" alt="Versus" src="https://upload.wikimedia.org/wikipedia/commons/7/70/Street_Fighter_VS_logo.png" />
           </div>
           <div class="center col-md-5">
-            <a target="_blank" rel="noopener noreferrer" href={data[1].url}><img alt={data[1].title} src={(data[1].image == null)? "https://i.imgur.com/OzRR1xv.jpg" : data[1].image.slice(0, -35)} class="thing"/></a>
+            <a target="_blank" rel="noopener noreferrer" href={data[1].url}><img alt={data[1].title} src={(data[1].image == null)? "https://i.imgur.com/OzRR1xv.jpg" : data[1].image.slice(0, -34)} class="thing"/></a>
             <button disabled={disabled} type="button" onClick={() => vote(data[0].id, data[1].id, false)}>
               {data[1].title}
             </button>
